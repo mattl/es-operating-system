0d2c62f2b828e427547ac328b9783e2ba7f4a829
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2008-06-15 05:42:33 +0000

    Fix the file header.

--- os/libes++/src/posix_video.cpp
+++ os/libes++/src/posix_video.cpp
@@ -3,6 +3,7 @@
  * Copyright 2008 Chis Dan Ionut
  * Copyright 2006, 2007 Nintendo Co., Ltd.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
