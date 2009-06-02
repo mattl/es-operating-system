514afeeedcc6ae2545a1f180c37681cc6d465d7a
plk1986 <plk1986@gmail.com>
2009-06-02 11:28:45 +0000

    delete non Used directory

--- kala/copy1/esjs/scripts/gradient.js
+++
@@ -1,9 +0,0 @@
-var ctx = CanvasRenderingContext2D(root.lookup("/device/canvas"));
-
-var gradObj = ctx.createLinearGradient(0, 0, 320, 240);
-gradObj.addColorStop(0.3, "#ff0000");
-gradObj.addColorStop(0.6, "#0000ff");
-
-ctx.fillGradient = CanvasGradient(gradObj);
-ctx.rect(10, 10, 300, 220);
-ctx.fill();
--- kala/copy1/esjs/testsuite/72.js
+++
@@ -1,8 +0,0 @@
-x = 0;
-c = new Boolean(true);
-c.toString();
-if (c.valueOf())
-{
-    x = 1;
-}
-x;
