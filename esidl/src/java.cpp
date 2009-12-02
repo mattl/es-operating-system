mattl/es-operating-system

commit 6299a1a6cfa43923c162cbac21078ad3ca060ef1

(Java) : Fix scope name resolution.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@1380 d15c3b8f-5b43-0410-be93-4d5a55ce2954

esidl/src/java.cpp
@@ -103,6 +103,10 @@ public:
 
     virtual void at(const Interface* node)
     {
+        if (!currentNode)
+        {
+            currentNode = node->getParent();
+        }
         assert(!(node->getAttr() & Interface::Supplemental) && !node->isLeaf());
         writetab();
         if (node->getJavadoc().size())
@@ -150,10 +154,13 @@ public:
                 continue;
             }
             writeln("// %s", (*i)->getName().c_str());
+            const Node* saved = currentNode;
             for (NodeList::iterator j = (*i)->begin(); j != (*i)->end(); ++j)
             {
+                currentNode = *i;
                 visitInterfaceElement(*i, *j);
             }
+            currentNode = saved;
         }
 
         writeln("}");
@@ -190,13 +197,8 @@ public:
     {
         if (node->isTypedef(node->getParent()))
         {
-            return;
+            node->getSpec()->accept(this);
         }
-
-        writetab();
-        write("public ");
-        node->getSpec()->accept(this);
-        write(" %s;\n", node->getName().c_str());
     }
 
     virtual void at(const ArrayDcl* node)
@@ -297,14 +299,14 @@ public:
     {
         assert(current);
 
-        if (node->isBaseObject())
-        {
-            return;
-        }
-        std::string name = node->getName();
         Node* resolved = node->search(current);
-        if (resolved)
+        node->check(resolved, "could not resolved %s.", node->getName().c_str());
+        if (dynamic_cast<Interface*>(resolved) || dynamic_cast<ExceptDcl*>(resolved))
         {
+            if (resolved->isBaseObject())
+            {
+                return;
+            }
             if (Module* module = dynamic_cast<Module*>(resolved->getParent()))
             {
                 if (prefixedName != module->getPrefixedName())
@@ -339,8 +341,10 @@ public:
             {
                 continue;
             }
+            current = *i;
             visitChildren(*i);
         }
+        current = node;
     }
 
     virtual void at(const Attribute* node)

esidl/include/java.h
@@ -60,25 +60,14 @@ protected:
             return;
         }
 
-        const Node* saved = currentNode;
         for (NodeList::iterator i = node->begin(); i != node->end(); ++i)
         {
-            if (!(*i)->isDefinedIn(source))
-            {
-                continue;
-            }
-            if ((*i)->isNative(node->getParent()))
-            {
-                continue;
-            }
             if (separater && i != node->begin())
             {
                 write("%s", separater);
             }
-            currentNode = (*i);
             (*i)->accept(this);
         }
-        currentNode = saved;
     }
 
     std::string getBufferName(const Node* spec)
@@ -99,7 +88,7 @@ public:
         source(source),
         objectTypeName("Object"),
         useExceptions(true),
-        currentNode(getSpecification()),
+        currentNode(0),
         paramCount(0),
         variadicParam(0)
     {
@@ -119,7 +108,20 @@ public:
 
     virtual void at(const ScopedName* node)
     {
-        write("%s", node->getIdentifier().c_str());
+        Node* resolved = node->search(currentNode);
+        node->check(resolved, "%s could not resolved.", node->getName().c_str());
+        if (!dynamic_cast<Interface*>(resolved) && !dynamic_cast<ExceptDcl*>(resolved))
+        {
+            resolved->accept(this);
+        }
+        else if (resolved->isBaseObject())
+        {
+            write("Object");
+        }
+        else
+        {
+            write("%s", resolved->getName().c_str());
+        }
     }
 
     virtual void at(const Module* node)

esidl/npapi/include/w3c/cssomview.idl
@@ -11,6 +11,9 @@ typedef dom::Element Element;
 typedef events::MouseEvent MouseEvent;
 typedef ranges::Range Range;
 
+typedef views::ClientRectList ClientRectList;
+typedef views::ClientRect ClientRect;
+
 module views 
 {
 
@@ -61,6 +64,20 @@ interface Screen {
 
 Document implements DocumentView;
 
+interface ClientRectList {
+  readonly attribute unsigned long length;
+  getter ClientRect item(unsigned long index);
+};
+
+interface ClientRect {
+  readonly attribute float top;
+  readonly attribute float right;
+  readonly attribute float bottom;
+  readonly attribute float left;
+  readonly attribute float width;
+  readonly attribute float height;
+};
+
 };
 
 module dom 
@@ -102,20 +119,6 @@ module ranges
 [Supplemental] interface Range {
   ClientRectList getClientRects();
   ClientRect getBoundingClientRect();
-};
-
-interface ClientRectList {
-  readonly attribute unsigned long length;
-  getter ClientRect item(unsigned long index);
-};
-
-interface ClientRect {
-  readonly attribute float top;
-  readonly attribute float right;
-  readonly attribute float bottom;
-  readonly attribute float left;
-  readonly attribute float width;
-  readonly attribute float height;
 };
 
 };

esidl/npapi/include/w3c/geolocation.idl
@@ -6,6 +6,7 @@
 module geolocation
 {
 
+ typedef dom::DOMTimeStamp DOMTimeStamp;
  typedef html::Navigator Navigator;
 
  [NoInterfaceObject]

esidl/npapi/include/w3c/html5.idl
@@ -8,6 +8,7 @@
 #include "events.idl"
 #include "css.idl"
 #include "ranges.idl"
+#include "views.idl"
 
 module html
 {
@@ -16,10 +17,14 @@ typedef object Date;      // TBD at W3C
 typedef object FileList;  // see File API for the actual definition
 
 typedef dom::DOMImplementation DOMImplementation;
+typedef dom::Node Node;
 typedef dom::NodeList NodeList;
+typedef dom::DOMStringList DOMStringList;
 typedef events::Event Event;
 typedef events::EventTarget EventTarget;
 typedef stylesheets::LinkStyle LinkStyle;
+typedef css::CSSStyleDeclaration CSSStyleDeclaration;
+typedef views::AbstractView AbstractView;
 
 interface HTMLCollection {
   readonly attribute unsigned long length;

esidl/src/esidl.cpp
@@ -558,8 +558,8 @@ Node* Node::search(const std::string& elem, size_t pos) const
         n = elem.size();
     }
     for (NodeList::reverse_iterator i = children->rbegin();
-            i != children->rend();
-            ++i)
+         i != children->rend();
+         ++i)
     {
         if ((*i)->getName().compare(0, (*i)->getName().size(), elem, pos, n - pos) == 0)
         {

commit 32620ffd2f9dc61268f980018701085994c22f0f

(Java) : Fix to generate exception class member fields. Regression in r1380.

esidl/src/java.cpp
@@ -199,6 +199,14 @@ public:
         {
             node->getSpec()->accept(this);
         }
+        else
+        {
+            // This node is an exception class member.
+            writetab();
+            write("public ");
+            node->getSpec()->accept(this);
+            write(" %s;\n", node->getName().c_str());
+        }
     }
 
     virtual void at(const ArrayDcl* node)