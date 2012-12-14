cdec6bc6b18099a545fbf33685ca21382513efee
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2012-12-14 09:03:25 +0000

    esidl 0.2.2

--- escort/testdata/script-002.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('t').addEventListener(
-  'click',
-  function(e) {
-    e.target.style.backgroundColor = 'cyan';
-  }
-);
--- esjs/scripts/clear.js
+++
@@ -1,4 +0,0 @@
-var c = CanvasRenderingContext2D(root.lookup("/device/canvas"));
-
-c.fillStyle = "rgba(255, 255, 255, 1)";
-c.fillRect(0, 0, 1024, 768);
